d1bfc7a28249763a6585075c9e0d50371fb6eae7
yeongminkang <yeongmin.kang@cfind.co.kr>
2018-11-02 10:28:56 +0900

    폰트사용 재정의

--- js/script.js
+++ js/script.js
@@ -9,4 +9,38 @@ $(function(){
       $('#header').removeClass('active');
     }
   });
+  
+  // contents lazyload effect
+  $(window).scroll(function () {
+    $('.lazyload1').each(function (i) {
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight()
+      var bottom_of_window = $(window).scrollTop() + $(window).height()
+      if (bottom_of_window > bottom_of_object) {
+        $(this).animate({
+          'opacity': '1',
+          'top': 0,
+        }, 500)
+      }
+    });
+    $('.lazyload2').each(function (i) {
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight()
+      var bottom_of_window = $(window).scrollTop() + $(window).height()
+      if (bottom_of_window > bottom_of_object) {
+        $(this).animate({
+          'opacity': '1',
+          'top': 0,
+        }, 800)
+      }
+    });
+    $('.lazyload3').each(function (i) {
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight()
+      var bottom_of_window = $(window).scrollTop() + $(window).height()
+      if (bottom_of_window > bottom_of_object) {
+        $(this).animate({
+          'opacity': '1',
+          'top': 0,
+        }, 1100)
+      }
+    });
+  })
 })
